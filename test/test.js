ae31c2a0bff2f3412899038f4d4204f14eb2f9c2
Dusty Little <dlittle@toyatech.net>
2014-08-15 11:43:52 -0500

    add some basic tests

--- test/test.js
+++ test/test.js
@@ -1,11 +1,43 @@
-var fs = require('fs'),
-  esprima = require('esprima'),
-  tv4 = require('tv4');
+var assert = require('assert')
+  , fs = require('fs')
+  , tv4 = require('tv4')
+  , esprima = require('esprima')
+  , esschema = require('../esschema.json')
 
-var schema = JSON.parse(fs.readFileSync(__dirname + '/esschema.json'));
-var data = JSON.stringify(esprima.parse(fs.readFileSync(__dirname + '/test/mongoose.js')));
-
-
-console.log(tv4.validate(data, schema));
-
-console.log(tv4.error);
+describe('esschema.json', function() {
+  it('should be in valid JSON format', function() {
+    fs.readFile(__dirname + '/../esschema.json', 'utf8', function (err, data) {
+      assert.ifError(err);
+      assert.doesNotThrow(function() {
+        JSON.parse(data);
+      })
+    })
+  })
+  it ('should be in valid JSON Schema format', function() {
+    tv4.validate(
+      esschema, 
+      'http://json-schema.org/schema#', 
+      function(isValid, validationError) {
+        assert(isValid);
+        assert.equal(validationError, undefined);
+      }
+    )
+  })
+  fs.readdir(__dirname + '/fixtures/code', function(err, files) {
+    assert.ifError(err);
+    files.forEach(function(file) {
+      it('should validate ' + file, function() {
+        fs.readFile(__dirname + '/fixtures/code/' + file, function(err, data) {
+           tv4.validate(
+             esprima.parse(data),
+             esschema,
+             function(isValid, validationError) {
+               assert(isValid);
+               assert.equal(validationError, undefined);
+             }
+           ) 
+        })
+      })
+    })
+  })
+})

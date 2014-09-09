8b6a14ce2f5d83517a5a7aa63ebe43984602512d
Dusty Little <dlittle@toyatech.net>
2014-09-09 12:43:04 -0500

    cleanup in preparation for first release

--- test/test.js
+++ test/test.js
@@ -1,117 +1,192 @@
-var assert = require('assert')
-  , fs = require('fs')
-  , tv4 = require('tv4')
-  , esschema = require('../esschema.json')
-  , Empty = require('./fixtures/Empty.json')
-  , FortyTwo = require('./fixtures/FortyTwo.json')
-
-function valid(data, done) {
-  var result = tv4.validateResult(data, esschema)
-  if (result.error) {
-    fs.writeFile(
-      './error.json', 
-      JSON.stringify(result.error, null, 2),  
-      function(err) {
-        console.err(err);
+var assert = require('assert');
+var fs = require('fs');
+var esprima = require('esprima');
+var tv4 = require('tv4');
+var esschema = require('../esschema.json');
+function validate(node, done) {
+    var data = esprima.parse(fs.readFileSync('./test/fixtures/' + node + '.js'));
+    var result = tv4.validateResult(data, esschema);
+    debugger;
+    assert(result.valid);
+    done();
+}
+describe('A Program node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('Program', done);
     });
-  }
-  assert.equal(result.valid, true);
-  done();
-}
-
-function invalid(data, expect, done) {
-  var result = tv4.validateResult(data, esschema)
-//  console.log(result.error.message);
-  assert.equal(result.valid, false);
-  assert.equal(result.error.code, expect.code);
-  assert.equal((result.error.message.indexOf(expect.message) > -1), true);
-  done();
-}
-
-function checkMessageMissingRequiredProperty(property) {
-  return 'Missing required property: ' + property;
-} 
-
-function checkMessageStringDoesNotMatchPattern(pattern) {
-  return 'String does not match pattern: ' + pattern
-}
-
-function checkMessageInvalidType(found, expected) {
-  return 'invalid type: ' + found + ' (expected ' + expected + ')';
-}
-
-function checkMessageDataDoesNotMatchAnySchemasFrom(keyword) {
-  return 'Data does not match any schemas from "' + keyword + '"';
-}
-
-function clone(object) {
-  return JSON.parse(JSON.stringify(object));
-}
-
-describe('esschema.json', function() {
-  describe('definitions', function() {
-    describe('Program', function() {
-      describe('#type', function() {
-        it('should be invalid when not \'Program\'', function(done) {
-          var data = clone(Empty);
-          data.type = 'Programm';
-          var expect = { 
-            code: tv4.errorCodes.STRING_PATTERN,
-            message: checkMessageStringDoesNotMatchPattern('^Program$')
-          };
-          invalid(data, expect, done);
-        });
-        it('should be invalid when not a string', function(done) {
-          var data = clone(Empty);
-          data.type = 1;
-          var expect = { 
-            code: tv4.errorCodes.INVALID_TYPE,
-            message: checkMessageInvalidType('number', 'string') 
-          };
-          invalid(data, expect, done);
-        });
-        it('should be required', function(done) {
-          var data = clone(Empty);
-          delete data.type;
-          var expect = { 
-            code: tv4.errorCodes.OBJECT_REQUIRED,
-            message: checkMessageMissingRequiredProperty('type')
-          };
-          invalid(data, expect, done);
-        });
-      });
-      describe('#body', function() {
-        it('should be invalid if not an array', function(done) {
-          var data = clone(Empty);
-          data.body = 1;
-          var expect = { 
-            code: tv4.errorCodes.INVALID_TYPE,
-            message: checkMessageInvalidType('number', 'array') 
-          };
-          invalid(data, expect, done);
-        });
-        it('should be an array of a valid type', function(done) {
-          var data = clone(FortyTwo);
-          data.body[0].type = 'VariableDeclarationn';
-          var expect = {
-            code: tv4.errorCodes.ONE_OF_MISSING,
-            message: checkMessageDataDoesNotMatchAnySchemasFrom('oneOf')
-          };
-          invalid(data, expect, done);
-        }); 
-      });
+});
+describe('An EmptyStatement node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('EmptyStatement', done);
     });
-  });
 });
-
-describe('Empty.json', function() {
-  it('should be valid against esschema.json', function(done) {
-    valid(Empty, done);
-  });
+describe('A BlockStatement node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('BlockStatement', done);
+    });
 });
-
-describe('FortyTwo.json', function() {
-  it('should be valid against esschema.json', function(done) {
-    valid(FortyTwo, done);
-  });
+describe('An ExpressionStatement node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('ExpressionStatement', done);
+    });
+});
+describe('An IfStatement node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('IfStatement', done);
+    });
+});
+describe('A LabeledStatement node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('LabeledStatement', done);
+    });
+});
+describe('A BreakStatement node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('BreakStatement', done);
+    });
+});
+describe('A ContinueStatement node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('ContinueStatement', done);
+    });
+});
+describe('A WithStatement node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('WithStatement', done);
+    });
+});
+describe('A SwitchStatement node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('SwitchStatement', done);
+    });
+});
+describe('A ReturnStatement node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('ReturnStatement', done);
+    });
+});
+describe('A ThrowStatement node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('ThrowStatement', done);
+    });
+});
+describe('A TryStatement node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('TryStatement', done);
+    });
+});
+describe('A WhileStatement node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('WhileStatement', done);
+    });
+});
+describe('A DoWhileStatement node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('DoWhileStatement', done);
+    });
+});
+describe('A ForStatement node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('ForStatement', done);
+    });
+});
+describe('A ForInStatement node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('ForInStatement', done);
+    });
+});
+describe('A DebuggerStatement node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('DebuggerStatement', done);
+    });
+});
+describe('A FunctionDeclaration node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('FunctionDeclaration', done);
+    });
+});
+describe('A VariableDeclaration node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('VariableDeclaration', done);
+    });
+});
+describe('A ThisExpression node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('ThisExpression', done);
+    });
+});
+describe('An ArrayExpression node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('ArrayExpression', done);
+    });
+});
+describe('An ObjectExpression node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('ObjectExpression', done);
+    });
+});
+describe('A FunctionExpression node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('FunctionExpression', done);
+    });
+});
+describe('A SequenceExpression node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('SequenceExpression', done);
+    });
+});
+describe('An UnaryExpression node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('UnaryExpression', done);
+    });
+});
+describe('A BinaryExpression node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('BinaryExpression', done);
+    });
+});
+describe('An AssignmentExpression node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('AssignmentExpression', done);
+    });
+});
+describe('An UpdateExpression node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('UpdateExpression', done);
+    });
+});
+describe('A LogicalExpression node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('LogicalExpression', done);
+    });
+});
+describe('A ConditionalExpression node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('ConditionalExpression', done);
+    });
+});
+describe('A NewExpression node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('NewExpression', done);
+    });
+});
+describe('A CallExpression node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('CallExpression', done);
+    });
+});
+describe('A MemberExpression node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('MemberExpression', done);
+    });
+});
+describe('An ObjectPattern node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('ObjectPattern', done);
+    });
+});
+describe('An ArrayPattern node', function () {
+    it('should be valid against esschema.json', function (done) {
+        validate('ArrayPattern', done);
+    });
 });

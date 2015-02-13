e69f8a34968bb748f689a27b44c43c87bddbcf8c
Dusty Little <dlittle@toyatech.net>
2015-02-13 15:04:59 -0600

    add test for simple AST

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 var assert = require('assert')
+  , esprima = require('esprima')
   , tv4 = require('tv4')
   , esschema = require('../esschema')
   , schema = require('./fixtures/schema.json')
@@ -9,6 +10,14 @@ describe('esschema', function() {
     it('should generate a valid JSON Schema', function() {
       tv4.addSchema(schema);
       var s = esschema.generate(ast)
+        , r = tv4.validateResult(s, schema);
+      console.log(JSON.stringify(r, null, 2));
+      assert(r.valid);
+    })
+    it('should generate a valid JSON Schema from simple AST', function() {
+      tv4.addSchema(schema);
+      var a = esprima.parse('')
+        , s = esschema.generate(a)
         , r = tv4.validateResult(s, schema);
       console.log(JSON.stringify(r, null, 2));
       assert(r.valid);
